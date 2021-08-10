c60fcd768860bf9c57cfca25b3df4ac848f0cd52
LanternServer <87125248+LanternServer@users.noreply.github.com>
2021-08-10 12:07:44 +0100

    Add files via upload
    Added stuff

---
+++ lanternMCsource/lanternMC/src/LanternServer/lanternMC/Main.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LanternServer\lanternMC
+
+use pocketmine\plugin\PluginBase
+use pocketmine\plugin\commandSender
+use pocketmine\command\Command;
+
+class Main extends PluginBase {
+
+    public function onEnable() {
+      $this->getServer()->getLogger()->info("[LanternMC custom plugin enabled!]")
+    }
+
+    public function onDisable() {
+      $this->getServer()->getLogger()->info("[LanternMC custom plugin disabled..]")
+    }
+
+    public function onCommand(commandSender $sender, Command $command, string $label, array $args) : bool {
+      switch ($command->getName()) {
+        case "lanternMC":
+        $sender->sendMessage("[§6Lantern§4MC]§rWelcome to §6Lantern§4MC§r!");
+      }
+      return true;
+    }
+
+  }
